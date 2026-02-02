e76741c354a3890cffeec668f5bfc3da7aa8ee6a
zakky <fakhrimuzakki06@gmail.com>
2026-02-02 15:52:21 +0700

    add: branch day-6, add filter feature and implement callback function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,10 @@
 import Notfound from "./components/Notfound.js";
 import ProjectCard from "./components/ProjectCard.js";
-import { fileToBase64, addProject } from "./services/projectService.js";
+import {
+  fileToBase64,
+  addProject,
+  filterByLastTime,
+} from "./services/projectService.js";
 import { validateImage } from "./utils/validateImage.js";
 
 // ambil element
@@ -8,15 +12,18 @@ const projectsContainer = document.getElementById("projects");
 const form = document.getElementById("projectForm");
 const image = document.getElementById("image");
 const imagePreview = document.getElementById("imagePreview");
+const filterByLastTimeMenu = document.getElementById("filterByLastTimeMenu");
 
 let projectData = [];
 let projectId = 1;
 
 // Function yang akan dijalankna ketika terjadi perubahan pada variabel projectData
-const renderProjects = function () {
+const renderProjects = function (dataParams) {
   projectsContainer.innerHTML = "";
 
-  projectData.forEach((project, index) => {
+  const data = dataParams ? dataParams : projectData;
+
+  data.forEach((project, index) => {
     const card = document.createElement("div");
     card.className = "col-md-4";
 
@@ -36,6 +43,12 @@ const handleSubmit = async function (event) {
   const technologies = formData.getAll("technologies");
   const raw = Object.fromEntries(formData.entries());
 
+  const currentTime = new Date();
+  const date = {
+    createdAt: currentTime,
+    updatedAt: currentTime,
+  };
+
   const imageFile = formData.get("image");
 
   // binary menjadi string
@@ -47,28 +60,30 @@ const handleSubmit = async function (event) {
     ...raw,
     technologies: technologies,
     image: imageBase64,
+    ...date,
   };
 
   // Menyimpan datanya ke local storage
-  addProject(userInput);
-  projectData.push(userInput);
-  renderProjects();
+  addProject(userInput, () => {
+    projectData.push(userInput);
+    renderProjects();
 
-  // Supaya inputnya kosong lagi
-  imagePreview.classList.add("d-none");
-  form.reset();
+    // Supaya inputnya kosong lagi
+    imagePreview.classList.add("d-none");
+    form.reset();
 
-  Toastify({
-    text: "Project successfully added!",
-    duration: 3000,
-    gravity: "top",
-    position: "right",
-    close: true,
-    style: {
-      background: "#198754",
-      borderRadius: "10px",
-    },
-  }).showToast();
+    Toastify({
+      text: "Project successfully added!",
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      close: true,
+      style: {
+        background: "#198754",
+        borderRadius: "10px",
+      },
+    }).showToast();
+  });
 };
 
 let imagePreviewUrl = "";
@@ -111,5 +126,22 @@ const loadProjectData = function () {
 
 form.addEventListener("submit", handleSubmit);
 image.addEventListener("change", handleChange);
+
+filterByLastTimeMenu.addEventListener("click", (event) => {
+  const button = event.target.closest("button[data-filter]");
+  if (!button) return;
+
+  const dataSet = button.dataset.filter; // 5
+  const result = filterByLastTime(projectData, dataSet);
+
+  if (!result.length) {
+    projectsContainer.innerHTML = Notfound(
+      "Data is empty",
+      "You haven't added data yet",
+    );
+  } else {
+    renderProjects(result);
+  }
+});
 
 loadProjectData();
--- assets/scripts/services/projectService.js
+++ assets/scripts/services/projectService.js
@@ -8,9 +8,28 @@ export const fileToBase64 = function (file) {
   });
 };
 
-export const addProject = function (project) {
+export const addProject = function (project, callback) {
   const projects = JSON.parse(localStorage.getItem("projects")) || [];
   projects.push(project);
 
   localStorage.setItem("projects", JSON.stringify(projects));
+  callback();
+};
+
+export const filterByLastTime = (projectData, minutes) => {
+  const now = Date.now();
+  // sekarang - 5 * 60detik * 1 detik
+  const threshold = now - Number(minutes) * 60 * 1000;
+  // sekarang
+
+  return projectData.filter((p) => {
+    const createdAt = new Date(p.createdAt).getTime();
+
+    // Artinya data yang akan di tampilan itu adalah data yang di buat ... menit yang lalu
+    //  createdAt itu 4 detik yang lalu
+    // threshold itu 5 menit yang lalu
+    // waktu saat ini 10
+    // 6 >= 5
+    return createdAt >= threshold;
+  });
 };
